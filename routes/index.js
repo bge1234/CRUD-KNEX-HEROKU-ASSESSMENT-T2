bfe0b5e80c621fbac8d5356eccc17a0d02b5641f
Ben Ebel <bge1234@gmail.com>
2016-01-20 10:42:51 -0700

    finished first set of tests

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
 
 router.get('/', function(req, res, next) {
   res.redirect('/posts');
+});
+
+router.get('/posts', function(req, res, next) {
+  knex('posts').select().then(function(results) {
+    res.json({'SUCCESS': results});
+  });
+});
+
+router.post('/posts', function(req, res, next) {
+  knex('posts').insert(req.body).then(function(result) {
+    res.redirect('/posts');
+  });
+});
+
+router.get('/posts/:id', function(req, res, next) {
+  knex('posts').where('id', req.params.id).select().then(function(results) {
+    res.json({'SUCCESS': results[0]});
+  });
+});
+
+router.get('/posts/:id/edit', function(req, res, next) {
+  knex('posts').where('id', req.params.id).select().then(function(results) {
+    res.json({'SUCCESS': results});
+  });
+});
+
+router.post('/posts/:id', function(req, res, next) {
+  knex('posts').update(req.body).then(function(result) {
+    res.redirect('/posts/' + req.params.id);
+  });
+});
+
+router.post('/posts/:id', function(req, res, next) {
+  knex('posts').where('id', req.params.id).update(req.body).then(function(result) {
+    res.redirect('/posts/' + req.params.id);
+  });
+});
+
+router.post('/posts/:id/delete', function(req, res, next) {
+  knex('posts').where('id', req.params.id).del().then(function(result) {
+    res.redirect('/posts');
+  });
 });
 
 module.exports = router;
